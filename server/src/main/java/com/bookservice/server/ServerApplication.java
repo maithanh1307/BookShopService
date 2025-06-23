8e16ff952b0c17bdbad8f3fe9fe2ed019101deef
maithanh1307 <nguyentranmaithanh1307@gmail.com>
2025-06-23 15:06:08 +0700

    book entity

--- server/src/main/java/com/bookservice/server/ServerApplication.java
+++ server/src/main/java/com/bookservice/server/ServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class WebApplication {
+public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebApplication.class, args);
+		SpringApplication.run(ServerApplication.class, args);
 	}
 
 }
