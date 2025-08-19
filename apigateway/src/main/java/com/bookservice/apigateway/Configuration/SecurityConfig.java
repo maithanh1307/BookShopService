799322f0bbe29fd404194148443ff9625c18dbbe
maithanh1307 <nguyentranmaithanh1307@gmail.com>
2025-08-19 13:13:34 +0700

    authentication jwt with oauth2

---
+++ apigateway/src/main/java/com/bookservice/apigateway/Configuration/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.bookservice.apigateway.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableReactiveMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/api/v1/public/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(resourceServer -> resourceServer
+                        .jwt(Customizer.withDefaults())
+                );
+        return http.build();
+    }
+}
+
